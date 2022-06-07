71a4a61178384a6cefb8699310cbf5a0503e65eb
chetan-2002 <77783033+chetan-2002@users.noreply.github.com>
2022-06-07 11:31:39 +0530

    Day 1

---
+++ Day-1_Arrays_part-1/sort_an_array.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    void sortColors(vector<int> &nums)
+    {
+        int zero = 0, i = 0, j = nums.size() - 1;
+        while (i <= j)
+        {
+            if (nums[i] == 0)
+            {
+                swap(nums[i++], nums[zero++]);
+            }
+            else if (nums[i] == 2)
+            {
+                swap(nums[j--], nums[i]);
+            }
+            else
+                i++;
+        }
+    }
+};
